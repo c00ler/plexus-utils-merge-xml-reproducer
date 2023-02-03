0c66357939c02bd6d4be03073fe9fb0bac5d3c5a
Alexey Venderov <avenderov@gradle.com>
2023-02-03 21:56:05 +0100

    add test via Xpp3Dom.mergeXpp3Dom

--- src/test/java/com/github/avenderov/ReproducerTest.java
+++ src/test/java/com/github/avenderov/ReproducerTest.java
@@ -1,5 +1,6 @@
 package com.github.avenderov;
 
+import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
 import org.codehaus.plexus.util.xml.Xpp3DomUtils;
 import org.junit.jupiter.api.Test;
@@ -10,105 +11,122 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ReproducerTest {
 
+    public static final String PARENT = "" +
+        "<configuration>\n" +
+        "    <gradleEnterprise>\n" +
+        "        <plugins>\n" +
+        "            <plugin>\n" +
+        "                <groupId>org.apache.maven.plugins</groupId>\n" +
+        "                <artifactId>maven-compiler-plugin</artifactId>\n" +
+        "                <executions>\n" +
+        "                    <execution>\n" +
+        "                        <id>default-compile</id>\n" +
+        "                        <outputs>\n" +
+        "                            <notCacheableBecause>something</notCacheableBecause>\n" +
+        "                        </outputs>\n" +
+        "                    </execution>\n" +
+        "                </executions>\n" +
+        "            </plugin>\n" +
+        "            <plugin>\n" +
+        "                <groupId>org.apache.maven.plugins</groupId>\n" +
+        "                <artifactId>maven-surefire-plugin</artifactId>\n" +
+        "                <inputs>\n" +
+        "                    <properties>\n" +
+        "                        <property>\n" +
+        "                            <name>additionalProperty</name>\n" +
+        "                            <value>parent-additionalProperty</value>\n" +
+        "                        </property>\n" +
+        "                    </properties>\n" +
+        "                </inputs>\n" +
+        "            </plugin>\n" +
+        "        </plugins>\n" +
+        "    </gradleEnterprise>\n" +
+        "</configuration>\n";
+
+    public static final String CHILD = "" +
+        "<configuration>\n" +
+        "    <gradleEnterprise>\n" +
+        "        <plugins>\n" +
+        "            <plugin>\n" +
+        "                <groupId>org.apache.maven.plugins</groupId>\n" +
+        "                <artifactId>maven-compiler-plugin</artifactId>\n" +
+        "            </plugin>\n" +
+        "            <plugin>\n" +
+        "                <groupId>org.apache.maven.plugins</groupId>\n" +
+        "                <artifactId>maven-surefire-plugin</artifactId>\n" +
+        "                <inputs>\n" +
+        "                    <properties combine.children=\"append\">\n" +
+        "                        <property>\n" +
+        "                            <name>childAdditionalProperty</name>\n" +
+        "                            <value>child-additionalProperty</value>\n" +
+        "                        </property>\n" +
+        "                    </properties>\n" +
+        "                </inputs>\n" +
+        "            </plugin>\n" +
+        "        </plugins>\n" +
+        "    </gradleEnterprise>\n" +
+        "</configuration>";
+
+    public static final String EXPECTED_RESULT = "" +
+        "<configuration>\n" +
+        "  <gradleEnterprise>\n" +
+        "    <plugins>\n" +
+        "      <plugin>\n" +
+        "        <groupId>org.apache.maven.plugins</groupId>\n" +
+        "        <artifactId>maven-compiler-plugin</artifactId>\n" +
+        "        <executions>\n" +
+        "          <execution>\n" +
+        "            <id>default-compile</id>\n" +
+        "            <outputs>\n" +
+        "              <notCacheableBecause>something</notCacheableBecause>\n" +
+        "            </outputs>\n" +
+        "          </execution>\n" +
+        "        </executions>\n" +
+        "      </plugin>\n" +
+        "      <plugin>\n" +
+        "        <groupId>org.apache.maven.plugins</groupId>\n" +
+        "        <artifactId>maven-surefire-plugin</artifactId>\n" +
+        "        <inputs>\n" +
+        "          <properties combine.children=\"append\">\n" +
+        "            <property>\n" +
+        "              <name>additionalProperty</name>\n" +
+        "              <value>parent-additionalProperty</value>\n" +
+        "            </property>\n" +
+        "            <property>\n" +
+        "              <name>childAdditionalProperty</name>\n" +
+        "              <value>child-additionalProperty</value>\n" +
+        "            </property>\n" +
+        "          </properties>\n" +
+        "        </inputs>\n" +
+        "      </plugin>\n" +
+        "    </plugins>\n" +
+        "  </gradleEnterprise>\n" +
+        "</configuration>\n";
+
     @Test
-    void shouldMergeConfigurations() throws Exception {
-        var lhs = "" +
-            "<configuration>\n" +
-            "    <gradleEnterprise>\n" +
-            "        <plugins>\n" +
-            "            <plugin>\n" +
-            "                <groupId>org.apache.maven.plugins</groupId>\n" +
-            "                <artifactId>maven-compiler-plugin</artifactId>\n" +
-            "                <executions>\n" +
-            "                    <execution>\n" +
-            "                        <id>default-compile</id>\n" +
-            "                        <outputs>\n" +
-            "                            <notCacheableBecause>something</notCacheableBecause>\n" +
-            "                        </outputs>\n" +
-            "                    </execution>\n" +
-            "                </executions>\n" +
-            "            </plugin>\n" +
-            "            <plugin>\n" +
-            "                <groupId>org.apache.maven.plugins</groupId>\n" +
-            "                <artifactId>maven-surefire-plugin</artifactId>\n" +
-            "                <inputs>\n" +
-            "                    <properties>\n" +
-            "                        <property>\n" +
-            "                            <name>additionalProperty</name>\n" +
-            "                            <value>parent-additionalProperty</value>\n" +
-            "                        </property>\n" +
-            "                    </properties>\n" +
-            "                </inputs>\n" +
-            "            </plugin>\n" +
-            "        </plugins>\n" +
-            "    </gradleEnterprise>\n" +
-            "</configuration>\n";
+    void shouldMergeConfigurationsUsingXpp3DomUtils() throws Exception {
+        var parentDom = toXpp3Dom(PARENT);
+        var childDom = toXpp3Dom(CHILD);
+        var expectedDom = toXpp3Dom(EXPECTED_RESULT);
 
-        var rhs = "" +
-            "<configuration>\n" +
-            "    <gradleEnterprise>\n" +
-            "        <plugins>\n" +
-            "            <plugin>\n" +
-            "                <groupId>org.apache.maven.plugins</groupId>\n" +
-            "                <artifactId>maven-compiler-plugin</artifactId>\n" +
-            "            </plugin>\n" +
-            "            <plugin>\n" +
-            "                <groupId>org.apache.maven.plugins</groupId>\n" +
-            "                <artifactId>maven-surefire-plugin</artifactId>\n" +
-            "                <inputs>\n" +
-            "                    <properties combine.children=\"append\">\n" +
-            "                        <property>\n" +
-            "                            <name>childAdditionalProperty</name>\n" +
-            "                            <value>child-additionalProperty</value>\n" +
-            "                        </property>\n" +
-            "                    </properties>\n" +
-            "                </inputs>\n" +
-            "            </plugin>\n" +
-            "        </plugins>\n" +
-            "    </gradleEnterprise>\n" +
-            "</configuration>";
+        var resultDom = Xpp3DomUtils.mergeXpp3Dom(parentDom, childDom);
 
-        var leftDom = Xpp3DomBuilder.build(new StringReader(lhs));
-        var rightDom = Xpp3DomBuilder.build(new StringReader(rhs));
+        assertEquals(expectedDom, resultDom);
+    }
 
-        var resultDom = Xpp3DomUtils.mergeXpp3Dom(leftDom, rightDom, null);
+    @Test
+    void shouldMergeConfigurationsUsingXpp3Dom() throws Exception {
+        var parentDom = toXpp3Dom(PARENT);
+        var childDom = toXpp3Dom(CHILD);
+        var expectedDom = toXpp3Dom(EXPECTED_RESULT);
 
-        var expectedResult = "" +
-            "<configuration>\n" +
-            "  <gradleEnterprise>\n" +
-            "    <plugins>\n" +
-            "      <plugin>\n" +
-            "        <groupId>org.apache.maven.plugins</groupId>\n" +
-            "        <artifactId>maven-compiler-plugin</artifactId>\n" +
-            "        <executions>\n" +
-            "          <execution>\n" +
-            "            <id>default-compile</id>\n" +
-            "            <outputs>\n" +
-            "              <notCacheableBecause>something</notCacheableBecause>\n" +
-            "            </outputs>\n" +
-            "          </execution>\n" +
-            "        </executions>\n" +
-            "      </plugin>\n" +
-            "      <plugin>\n" +
-            "        <groupId>org.apache.maven.plugins</groupId>\n" +
-            "        <artifactId>maven-surefire-plugin</artifactId>\n" +
-            "        <inputs>\n" +
-            "          <properties combine.children=\"append\">\n" +
-            "            <property>\n" +
-            "              <name>additionalProperty</name>\n" +
-            "              <value>parent-additionalProperty</value>\n" +
-            "            </property>\n" +
-            "            <property>\n" +
-            "              <name>childAdditionalProperty</name>\n" +
-            "              <value>child-additionalProperty</value>\n" +
-            "            </property>\n" +
-            "          </properties>\n" +
-            "        </inputs>\n" +
-            "      </plugin>\n" +
-            "    </plugins>\n" +
-            "  </gradleEnterprise>\n" +
-            "</configuration>\n";
+        // The order of arguments is different
+        var resultDom = Xpp3Dom.mergeXpp3Dom(childDom, parentDom);
 
-        assertEquals(Xpp3DomBuilder.build(new StringReader(expectedResult)), resultDom);
+        assertEquals(expectedDom, resultDom);
+    }
+
+    private static Xpp3Dom toXpp3Dom(String xml) throws Exception {
+        return Xpp3DomBuilder.build(new StringReader(xml));
     }
 }
